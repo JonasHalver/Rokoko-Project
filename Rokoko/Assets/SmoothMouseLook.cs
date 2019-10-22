17883021d60f8785dd8b2c4214cd076a12d69fbd
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2019-10-22 11:10:57 +0200

    Updated script, added layers

--- Rokoko/Assets/SmoothMouseLook.cs
+++ Rokoko/Assets/SmoothMouseLook.cs
@@ -30,13 +30,18 @@ public class SmoothMouseLook : MonoBehaviour
 
     Quaternion originalRotation;
 
+    public LayerMask obstacleMask;
     private float y;
     private bool sliding;
     private float dist;
     RaycastHit hit;
+    Vector3 viablePosition;
 
     void Update()
     {
+        FindViablePosition();
+        
+
         if (Physics.Raycast(cam.transform.position, Vector3.down, out hit, Mathf.Infinity))
         {
             dist = Vector3.Distance(cam.transform.position, hit.point);
@@ -80,43 +85,43 @@ public class SmoothMouseLook : MonoBehaviour
             Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
             Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
 
-            if (dist < cam.transform.localPosition.y + 0.1f)
-            {
-                if (!sliding)
-                {
-                    sliding = true;
-                    y = rotAverageY;                    
-                }
-                else
-                {
-                    if (rotAverageY > y)
-                    {
-                        float p = Mathf.Clamp((rotAverageY - y) / (maximumY - y), 0, 0.35f);
-                        if (p < 0)
-                        {
-                            cam.transform.localPosition = new Vector3(1, cam.transform.localPosition.y, -10);
-                            y = 0;
-                            sliding = false;
-                        }
-                        else
-                        {
-                            cam.transform.localPosition = new Vector3(1, cam.transform.localPosition.y, Mathf.Lerp(-10, -2f, p * 3f));
-                        }
-                    }
-                    else
-                    {
-                        cam.transform.localPosition = new Vector3(1, cam.transform.localPosition.y, -10);
-                        y = 0;
-                        sliding = false;
-                    }
-                }
-            }
-            else
-            {
-                cam.transform.localPosition = new Vector3(1, cam.transform.localPosition.y, -10);
-                y = 0;
-                sliding = false;
-            }
+            //if (dist < cam.transform.localPosition.y + 0.1f)
+            //{
+            //    if (!sliding)
+            //    {
+            //        sliding = true;
+            //        y = rotAverageY;                    
+            //    }
+            //    else
+            //    {
+            //        if (rotAverageY > y)
+            //        {
+            //            float p = Mathf.Clamp((rotAverageY - y) / (maximumY - y), 0, 0.35f);
+            //            if (p < 0)
+            //            {
+            //                cam.transform.localPosition = new Vector3(1, cam.transform.localPosition.y, -10);
+            //                y = 0;
+            //                sliding = false;
+            //            }
+            //            else
+            //            {
+            //                cam.transform.localPosition = new Vector3(1, cam.transform.localPosition.y, Mathf.Lerp(-10, -2f, p * 3f));
+            //            }
+            //        }
+            //        else
+            //        {
+            //            cam.transform.localPosition = new Vector3(1, cam.transform.localPosition.y, -10);
+            //            y = 0;
+            //            sliding = false;
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    cam.transform.localPosition = new Vector3(1, cam.transform.localPosition.y, -10);
+            //    y = 0;
+            //    sliding = false;
+            //}
 
             transform.localRotation = originalRotation * xQuaternion * yQuaternion;
         }
@@ -176,6 +181,11 @@ public class SmoothMouseLook : MonoBehaviour
         originalRotation = transform.localRotation;
     }
 
+    private void LateUpdate()
+    {
+        cam.transform.localPosition = new Vector3(1, cam.transform.localPosition.y, viablePosition.z);
+    }
+
     public static float ClampAngle(float angle, float min, float max)
     {
         angle = angle % 360;
@@ -191,5 +201,22 @@ public class SmoothMouseLook : MonoBehaviour
             }
         }
         return Mathf.Clamp(angle, min, max);
+    }
+
+    void FindViablePosition()
+    {
+        Debug.DrawRay(transform.position, -transform.forward * 10);
+        RaycastHit hit1;
+        if (Physics.Raycast(transform.position, -transform.forward, out hit1, 10, obstacleMask))
+        {
+            Vector3 hitDir = (hit1.point - transform.position).normalized;
+            float hitDst = -Vector3.Distance(hit1.point, transform.position);
+            hitDst = Mathf.Clamp(hitDst, -10, -2);
+            viablePosition = new Vector3(0, 0, hitDst + 1);
+        }
+        else
+        {
+            viablePosition = new Vector3(0, 0, -10);
+        }
     }
 }
