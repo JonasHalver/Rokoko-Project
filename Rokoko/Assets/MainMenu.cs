f2cf6b525946080bbdb1bdbda95c9679c6a1c530
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2019-10-24 14:43:49 +0200

    main menu and pause menu

---
+++ Rokoko/Assets/MainMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    bool allowLoad = false, loadingStarted = false;
+    bool wannaSmash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wannaSmash && !loadingStarted)
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        loadingStarted = true;
+        yield return null;
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(1);
+        asyncOperation.allowSceneActivation = false;
+
+        while (!asyncOperation.isDone)
+        {
+            if (asyncOperation.progress >= 0.9f)
+            {
+                if (allowLoad)
+                    asyncOperation.allowSceneActivation = true;
+                else
+                    print("Ready");
+            }
+            yield return null;
+        }
+    }
+
+    public void WannaSmash()
+    {
+        wannaSmash = true;
+    }
+
+    public void Smash()
+    {
+        allowLoad = true;
+    }
+
+    public void DontSmash()
+    {
+        Application.Quit();
+    }
+}
---
+++ Rokoko/Assets/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused;
+    public GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            paused = !paused;
+
+        menu.SetActive(paused);
+
+        if (paused)
+            Time.timeScale = 0.1f;
+        else
+            Time.timeScale = 1;
+    }
+
+    public void Continue()
+    {
+        paused = false;
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
